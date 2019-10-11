4b2c1ee626d2525df2e61eaf8c19096273dcae66
Luiz Felipe <felipealvesalp@gmail.com>
2019-10-11 13:57:35 -0300

    Pages

---
+++ src/pages/main/index.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+//Importa a API
+import api from '../../services/api';
+
+export default class Main extends Component{
+    render(){
+        return <h1>Hello</h1>
+    }
+}
