3c5485d983340c65ef14ceae397ed88209fa8122
Luiz Felipe <felipealvesalp@gmail.com>
2019-10-12 17:42:41 -0300

    Searching API Products

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,7 +3,18 @@ import React, {Component} from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component{
+    //Metodo executado logo apos o componente ser mostrado em tela
+    componentDidMount(){
+        this.loadProducts()
+    }
+    //Buscando products da API
+    loadProducts = async () => {
+        const response = await api.get('/products')
+        //Mostra o array de produtos
+        console.log(response.data.docs)
+    }
+    
     render(){
-        return <h1>Hello</h1>
+        return <h1>Welcome</h1>
     }
 }
